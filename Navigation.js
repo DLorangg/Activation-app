b5f27cc98655be0bfbd47fd3c8c7251e1182bba6
DLorangg <lorang.damian.epet20@gmail.com>
2023-09-26 19:05:43 -0300

    Modificando navegacion

---
+++ Navigation.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//Screens
+
+const Stack = createNativeStackNavigator();
+
+function MyStack(){
+    return(
+        <NavigationContainer theme={MyTheme}>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
+                <Stack.Screen options={{headerShown: false}} name="Button" component={ButtonScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default function Navigation(){
+    return(
+        <MyStack />
+    )
+}
+const MyTheme = {
+    dark: false,
+    colors: {
+      background: '#fffff'
+    },
+};
