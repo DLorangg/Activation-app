aaebcb916a87ce7ea171a6d49f4f784958939c9b
DLorangg <lorang.damian.epet20@gmail.com>
2023-09-27 20:44:48 -0300

    Terminando Login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
-import { verificarCredenciales } from '../database/users';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import axios from 'axios';
 
 const LoginScreen = () => {
   const [username, setUsername] = useState('');
@@ -8,39 +8,80 @@ const LoginScreen = () => {
   const [error, setError] = useState('');
 
   const handleLogin = () => {
-    verificarCredenciales(username, password, (error, exitoso) => {
-      if (error) {
-        // Maneja los errores
-        console.error('Error al verificar credenciales:', error);
-      } else {
-        if (exitoso) {
-          // El inicio de sesión fue exitoso
-          // Realiza acciones necesarias, como la navegación a la página principal
+    // Validación básica de campos de entrada
+    if (!username || !passwd) {
+      setError('Por favor, completa todos los campos.');
+      return;
+    }
+
+    axios
+      .post('http://192.168.1.11:7007/login', { username, passwd }) // Cambia la dirección IP y el puerto si es necesario
+      .then(response => {
+        if (response.data.success) {
+          // Inicio de sesión exitoso
+          console.log('Inicio de sesión exitoso:', response.data.message);
         } else {
-          // El inicio de sesión falló, muestra un mensaje de error
-          setError('Credenciales incorrectas');
+          // Inicio de sesión fallido, muestra un mensaje de error
+          setError('Credenciales incorrectas.');
         }
-      }
-    });
+      })
+      .catch(error => {
+        console.error('Error al iniciar sesión:', error);
+        setError('Hubo un problema al iniciar sesión. Inténtalo de nuevo más tarde.');
+      });
   };
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Iniciar Sesión</Text>
       <TextInput
+        style={styles.input}
         placeholder="Nombre de usuario"
         value={username}
         onChangeText={text => setUsername(text)}
       />
       <TextInput
+        style={styles.input}
         placeholder="Contraseña"
         secureTextEntry={true}
         value={passwd}
         onChangeText={text => setPasswd(text)}
       />
-      <Button title="Iniciar sesión" onPress={handleLogin} />
-      {error ? <Text>{error}</Text> : null}
+      <Button
+        title="Iniciar sesión"
+        onPress={handleLogin}
+        color="#0E6AB0"
+      />
+      {error ? <Text style={styles.error}>{error}</Text> : null}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#0E6AB0',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    backgroundColor: '#fff',
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  error: {
+    color: 'red',
+    marginTop: 10,
+  },
+});
 
 export default LoginScreen;
