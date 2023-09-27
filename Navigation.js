426758bc0709bab586dc7300c2cb4ba50276b8b6
DLorangg <lorang.damian.epet20@gmail.com>
2023-09-27 20:49:36 -0300

    Desimportando

--- Navigation.js
+++ Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 //Screens
