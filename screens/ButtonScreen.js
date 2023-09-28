2f37c925589a60435773919094a7e6b96fe6b340
DLorangg <lorang.damian.epet20@gmail.com>
2023-09-28 15:50:34 -0300

    Aplicando seleccionador de zonas

--- screens/ButtonScreen.js
+++ screens/ButtonScreen.js
@@ -1,43 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, Modal } from 'react-native';
+import ZonePicker from '../components/ZonePicker'; // Importa el componente ZonePicker que creamos antes
 
 const ButtonScreen = ({ route }) => {
   const { user } = route.params;
+  const [showForm, setShowForm] = useState(false); // Estado para mostrar u ocultar el formulario
 
-  const handleShowForm = () => {
-    Alert.prompt(
-      'Ingrese sus datos',
-      null,
-      [
-        {
-          text: 'Cancelar',
-          onPress: () => console.log('Cancelado'),
-          style: 'cancel',
-        },
-        {
-          text: 'Enviar',
-          onPress: (data) => {
-            // Aquí puedes manejar los datos ingresados por el usuario
-            console.log('Datos ingresados:', data);
-          },
-        },
-      ],
-      'plain-text', // Tipo de entrada de texto
-      '', // Valor inicial del campo de texto
-      'numeric' // Teclado numérico
-    );
+  const handleButtonPress = () => {
+    setShowForm(true); // Muestra el formulario cuando se presiona el botón
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Bienvenido {user.name} {user.surname}!</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={handleShowForm} style={styles.button}>
-          <Image source={require('../assets/boton.png')} style={styles.buttonImage} />
-        </TouchableOpacity>
-      </View>
+      <Text style={styles.title}>Bienvenido {user.name} {user.surname}!</Text>
+
+      {/* Botón con la imagen */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleButtonPress}
+      >
+        <Image source={require('../assets/boton.png')} style={styles.buttonImage} />
+      </TouchableOpacity>
+
+      {/* Formulario desplegable */}
+      {showForm && <ZonePicker />}
+
     </View>
   );
 };
@@ -49,28 +36,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
   },
-  titleContainer: {
-    flex: 1,
-    justifyContent: 'center',
-  },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 20,
     color: '#0E6AB0',
   },
-  buttonContainer: {
-    flex: 1,
-    justifyContent: 'center',
-  },
   button: {
-    width: 200, // Ancho del botón
-    height: 200, // Alto del botón
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 20,
   },
   buttonImage: {
-    width: '100%',
-    height: '100%',
+    width: 100, // Ajusta el ancho de la imagen según tus preferencias
+    height: 100, // Ajusta la altura de la imagen según tus preferencias
   },
 });
 
